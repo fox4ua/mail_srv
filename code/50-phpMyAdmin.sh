a2bd93b84c9f59b38971f788486574960410e661
Fox4UA <fox4ua@gmail.com>
2023-03-21 00:06:04 +0200

    development

--- code/50-phpMyAdmin.sh
+++ code/50-phpMyAdmin.sh
@@ -9,3 +9,9 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-language
 unzip phpMyAdmin-5.2.1-all-languages.zip
 mv phpMyAdmin-5.2.1-all-languages pma
 sudo cp /var/www/pma/config.sample.inc.php /var/www/pma/config.inc.php
+
+echo '$CONF["configured"] = '{$RESULT}';' >> /var/www/html/postfixadmin/config.inc.php
+echo "$cfg['blowfish_secret'] = '';" >> /var/www/html/pma/config.inc.php
+ 
+ blowfish_secret=$(pwgen -s 32 1)
+echo $blowfish_secret
