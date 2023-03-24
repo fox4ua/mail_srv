b732ffc56a3b37256d2c07ef4ed979c0dc1acfda
ByHelp <support@byhelp.org>
2023-03-24 13:16:47 +0200

    development

--- setup/code/10-init.sh
+++ setup/code/10-init.sh
@@ -2,6 +2,6 @@
 # =================================================
 echo -e "\e[92m -========Install First step ...========- \e[39m"
 # =================================================
-apt-get install apt-transport-https vim mc unzip curl -y
+apt-get install apt-transport-https vim mc curl -y
 # =================================================
 echo -e "\e[94m -========End First step ...========- \e[39m"
--- setup/code/50-phpMyAdmin.sh
+++ setup/code/50-phpMyAdmin.sh
@@ -3,7 +3,6 @@
 echo -e "\e[92m -========Install phpMyAdmin ...========- \e[39m"
 # =================================================
 apt install pwgen
-
 mkdir -p /var/lib/phpmyadmin/tmp
 chown -R www-data:www-data /var/lib/phpmyadmin
 # cd /var/www/
@@ -11,11 +10,9 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-language
 unzip phpMyAdmin-5.2.1-all-languages.zip
 mv phpMyAdmin-5.2.1-all-languages /var/www/pma
 cp /var/www/pma/config.sample.inc.php /var/www/pma/config.inc.php
-
 blowfish_secret=$(pwgen -s 32 1)
 sed -i "s/blowfish_secret'] = '/blowfish_secret'] = '${blowfish_secret}/" /var/www/pma/config.inc.php
-echo "$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';" >> /var/www/pma/config.inc.php
-
+echo "\$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';" >> /var/www/pma/config.inc.php
 mv code/src/phpMyAdmin/phpmyadmin.conf /etc/apache2/conf-enabled/phpmyadmin.conf
 systemctl reload apache2
 
