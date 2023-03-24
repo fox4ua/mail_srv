4b4e438390127a615d163c54f70f86bdee1d8a23
ByHelp <support@byhelp.org>
2023-03-24 12:28:45 +0200

    development

--- setup/init.sh
+++ setup/init.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-
 echo -e "\e[92m...\e[39m"
-echo -e "\e[92mDownload and Installing mailx cyrus-sasl cyrus-sasl-lib cyrus-sasl-plain postfix  ...\e[39m"
+echo -e "\e[92mInstalling Mail Servert ...\e[39m"
 echo -e "\e[92m...\e[39m"
-
-
 chmod +x code/*.sh
 source data.sh
 ./code/10-init.sh
--- wget.sh
+++ wget.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo -e "\e[92m...\e[39m"
-echo -e "\e[92mDownload and Installing mailx cyrus-sasl cyrus-sasl-lib cyrus-sasl-plain postfix  ...\e[39m"
+echo -e "\e[92mDownload ...\e[39m"
 echo -e "\e[92m...\e[39m"
 
 wget https://github.com/fox4ua/mail_srv/archive/master.zip
 apt-get install unzip -y
 unzip master.zip
-cd /root/mail_srv-main/
-chmod +x init.sh
-./init.sh
+cd ./mail_srv-main/
+chmod +x ./setup/init.sh
+./setup/init.sh
