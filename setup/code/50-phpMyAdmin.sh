3c905bdab5a15e9b63c7d4a202cbda9173bd5560
ByHelp <support@byhelp.org>
2023-03-24 16:13:56 +0200

    development

--- setup/code/50-phpMyAdmin.sh
+++ setup/code/50-phpMyAdmin.sh
@@ -9,6 +9,7 @@ chown -R www-data:www-data /var/lib/phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.zip
 unzip phpMyAdmin-5.2.1-all-languages.zip
 mv phpMyAdmin-5.2.1-all-languages /var/www/pma
+chown -R www-data:www-data /var/www/pma
 cp /var/www/pma/config.sample.inc.php /var/www/pma/config.inc.php
 blowfish_secret=$(pwgen -s 32 1)
 sed -i "s/blowfish_secret'] = '/blowfish_secret'] = '${blowfish_secret}/" /var/www/pma/config.inc.php
