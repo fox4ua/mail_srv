55dd36b9c39c97494f3d2e44c4beff909aefce65
Fox4UA <fox4ua@gmail.com>
2023-03-25 00:07:05 +0200

    development

--- setup/code/50-phpMyAdmin.sh
+++ setup/code/50-phpMyAdmin.sh
@@ -8,13 +8,12 @@ chown -R www-data:www-data /var/lib/phpmyadmin
 # cd /var/www/
 wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.zip
 unzip phpMyAdmin-5.2.1-all-languages.zip
-mv phpMyAdmin-5.2.1-all-languages /var/www/pma
-chown -R www-data:www-data /var/www/pma
-cp /var/www/pma/config.sample.inc.php /var/www/pma/config.inc.php
+mv phpMyAdmin-5.2.1-all-languages /var/www/postfixadmin
+chown -R www-data:www-data /var/www/postfixadmin
+cp /var/www/postfixadmin/config.sample.inc.php /var/www/postfixadmin/config.inc.php
 blowfish_secret=$(pwgen -s 32 1)
-sed -i "s/blowfish_secret'] = '/blowfish_secret'] = '${blowfish_secret}/" /var/www/pma/config.inc.php
-echo "\$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';" >> /var/www/pma/config.inc.php
+sed -i "s/blowfish_secret'] = '/blowfish_secret'] = '${blowfish_secret}/" /var/www/postfixadmin/config.inc.php
+echo "\$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';" >> /var/www/postfixadmin/config.inc.php
 mv code/src/phpMyAdmin/phpmyadmin.conf /etc/apache2/conf-enabled/phpmyadmin.conf
 systemctl reload apache2
-
 # =================================================
