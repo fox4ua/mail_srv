2ababa2f9189d8085bfe95b90b6dc6fd9e456136
Fox4UA <fox4ua@gmail.com>
2023-03-25 13:38:19 +0200

    add postfixadmin

--- setup/code/50-phpMyAdmin.sh
+++ setup/code/50-phpMyAdmin.sh
@@ -8,12 +8,12 @@ chown -R www-data:www-data /var/lib/phpmyadmin
 # cd /var/www/
 # wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.zip
 # unzip phpMyAdmin-5.2.1-all-languages.zip
-mv code/src/phpMyAdmin/files /var/www/postfixadmin
-chown -R www-data:www-data /var/www/postfixadmin
-cp /var/www/postfixadmin/config.sample.inc.php /var/www/postfixadmin/config.inc.php
+mv code/src/phpMyAdmin/files /var/www/phpmyadmin
+chown -R www-data:www-data /var/www/phpmyadmin
+cp /var/www/phpmyadmin/config.sample.inc.php /var/www/phpmyadmin/config.inc.php
 blowfish_secret=$(pwgen -s 32 1)
-sed -i "s/blowfish_secret'] = '/blowfish_secret'] = '${blowfish_secret}/" /var/www/postfixadmin/config.inc.php
-echo "\$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';" >> /var/www/postfixadmin/config.inc.php
+sed -i "s/blowfish_secret'] = '/blowfish_secret'] = '${blowfish_secret}/" /var/www/phpmyadmin/config.inc.php
+echo "\$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';" >> /var/www/phpmyadmin/config.inc.php
 mv code/src/phpMyAdmin/phpmyadmin.conf /etc/apache2/conf-enabled/phpmyadmin.conf
 systemctl reload apache2
 # =================================================
--- setup/code/60-postfixAdmin.sh
+++ setup/code/60-postfixAdmin.sh
@@ -2,13 +2,14 @@
 # =================================================
 echo -e "\e[92m -========Install PostfixAdmin ...========- \e[39m"
 # =================================================
+mv code/src/postfixAdmin/files /var/www/postfixadmin
+chown -R www-data:www-data /var/www/postfixadmin
 
 
-mv /var/www/postfixadmin-postfixadmin-3.3.11 /var/www/postfixadmin
 
 PASSWORD_DEFAULT=$(php -r "echo password_hash('some password here', PASSWORD_DEFAULT);")
 
 
-mkdir -p /srv/postfixadmin/templates_c
-chown -R www-data:www-data /srv/postfixadmin/templates_c
+mkdir -p /var/www/postfixadmin/templates_c
+chown -R www-data:www-data /var/www/postfixadmin/templates_c
 
