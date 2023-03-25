d22a49570ad78ce698a5c35fb7ff583dc57f532a
Fox4UA <fox4ua@gmail.com>
2023-03-25 11:49:28 +0200

    add phpMyAdmin

--- setup/code/60-postfixAdmin.sh
+++ setup/code/60-postfixAdmin.sh
@@ -2,3 +2,13 @@
 # =================================================
 echo -e "\e[92m -========Install PostfixAdmin ...========- \e[39m"
 # =================================================
+
+
+mv /var/www/postfixadmin-postfixadmin-3.3.11 /var/www/postfixadmin
+
+PASSWORD_DEFAULT=$(php -r "echo password_hash('some password here', PASSWORD_DEFAULT);")
+
+
+mkdir -p /srv/postfixadmin/templates_c
+chown -R www-data:www-data /srv/postfixadmin/templates_c
+
